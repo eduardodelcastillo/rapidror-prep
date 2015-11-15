615e7027540cd6e08e612039e4f914c7da39f3da
Eduardo del Castillo <jdcastle4@gmail.com>
2015-11-15 15:56:48 +0700

    Prep for Course 2

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_many :posts
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+
+  has_and_belongs_to_many :groups                    
+end
